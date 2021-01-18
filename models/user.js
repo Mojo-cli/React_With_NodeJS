70c05484e3db06932294f2f05a937d58c29fb807
Mojo-cli <adwait420gaikwad@gmail.com>
2021-01-18 12:25:50 +0530

    password hashing

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -11,5 +12,39 @@ const userSchema = new mongoose.Schema({
         required:true,
     }
 })
+
+userSchema.pre('save', function(){
+    const user = this;
+    if(!user.isModified('pass')){
+        return next()
+    }
+    bcrypt.genSalt(10,(err, salt)=>{
+        if(err){
+            return next(err)
+        }
+        bcrypt.hash(user.pass,salt,(err, hash)=>{
+            if(err){
+                return next(err)
+            }
+            user.pass = hash;
+            return next();
+        })
+    })
+})
+
+userSchema.methods.comparePass = function (candidatePass) {
+    const user = this;
+    return new Promise((resolve,reject)=>{
+        bcrypt.compare(candidatePass,user.pass,(err,isMatch)=>{
+            if(err){
+                return reject(err)
+            }
+            if(!isMatch){
+                return reject(err)
+            }
+            resolve(true)
+        })
+    })
+}
 
 mongoose.model('User', userSchema);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,22 +3,47 @@ const mongoose = require('mongoose')
 const jwt = require("jsonwebtoken")
 const {jwtkey} = require('../keys')
 const router = express.Router();
-const User = mongoose.model('User'); 
+const User = mongoose.model('User');
+ 
 
 
 router.post('/signup',async (req,res)=>{
         const{email, pass} = req.body;
 
         try{
+
             const user = new User({email,pass});
             await user.save();
             const token = jwt.sign({userId:user._id}, jwtkey)
             res.send({token})
 
         }catch(err){
+
            return res.status(422).send(err.message)
-        }      
+
+        }    
+
     }
+
 )
+
+router.post('/signin',async (req,res)=>{
+    const {email,pass} = req.body
+    if(!email || !pass){
+        return res.status(422).send({error:"Must Provoide Email or Password "})
+    }
+    const user = await User.findOne({email})
+    if(!user){
+        return res.status(422).send({error:"Must Provoide Email or Password "})
+    }
+    try{
+        await user.comparePass(pass);
+        const token = jwt.sign({userId:user._id},jwtkey)
+        res.send({token})
+    }catch(err){
+        return res.status(422).send({error:"Must Provoide Email or Password "})
+    }
+
+})
 
 module.exports = router
